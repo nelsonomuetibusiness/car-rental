bad946c33ba3319fa0a276d0e62191a3276ef8f3
nelsonomuetibusiness <nelson.omueti.business@gmail.com>
2024-03-14 10:36:41 +0100

    updated commit

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -12,7 +12,11 @@ import { useRouter } from "next/navigation"
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <div className="flex justify-between">
     <Image src="/magnifying-glass.svg" alt="magnifying glass" width={40} height={40} className="object-contain" />
+    <p className="font-bold mt-2">search</p>
+    </div>
+    
   </button>
 )
 
@@ -61,17 +65,17 @@ const SearchBar = () => {
       <div className="searchbar__item">
         <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
 
-        <SearchButton otherClasses="sm:hidden" />
+        
       </div>
 
       <div>
         <Image src="/model-icon.png" alt="car model" width={25} height={25} className="absolute w-[20px] h-[20px] ml-4" />
 
         <input type="text" name="model" value={model} onChange={(e) => setModel(e.target.value)} placeholder="Tuareg" className="searchbar__input" />
-        <SearchButton otherClasses="sm:hidden" />
+        
 
       </div>
-      <SearchButton otherClasses="max-sm:hidden" />
+      <SearchButton otherClasses="" />
     </form>
   )
 }
